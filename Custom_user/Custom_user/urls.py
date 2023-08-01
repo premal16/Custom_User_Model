ec4909c7e2247ba560f480b57bc881b60ea5d24f
premal <premal@moontechnolabs.com>
2023-08-01 17:14:07 +0530

    add token authentication with user profile

--- Custom_user/Custom_user/urls.py
+++ Custom_user/Custom_user/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from api.views import RegisterView, LoginView
+from api.views import RegisterView, LoginView,UserProfile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('profile/', UserProfile.as_view(), name='profile'),
     path('api-auth/', include('rest_framework.urls')),
 
 ]
--- Custom_user/api/views.py
+++ Custom_user/api/views.py
@@ -7,14 +7,20 @@ from django.contrib.auth import login
 from .models import CustomUser
 from .serializers import CustomUserSerializer,UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-# CustomUser = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = CustomUserSerializer
 
+
+# @csrf_exempt
+
+# @method_decorator(csrf_exempt, name='dispatch')
 class LoginView(APIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
@@ -33,3 +39,10 @@ class LoginView(APIView):
             }
             return Response(data, status=200)
         return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserProfile(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        user = request.user 
+        return Response({'username': user.username, 'email': user.email})
